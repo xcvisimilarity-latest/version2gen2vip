ee4d66b4502e95094524d28b7cb54b0ba88caf91
xcvisimilarity-latest <xcvisimilarity@gmail.com>
2025-11-15 07:34:32 +0700

    update port to 3008 via Telegram bot

--- api/connect.js
+++ api/connect.js
@@ -28,7 +28,7 @@ export const INFO_TEXT = "Promo up reseller 60k pm 6283898206223";
 
 const config = {
   domain: "http://similarityyyyy.kantinvps.my.id",
-  port: 3004,
+  port: 3008,
   creator: "Wanz Official",
 };
 const base = `${config.domain}:${config.port}`;
